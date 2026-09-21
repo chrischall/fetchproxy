c7eda66f796e755636db0117835fcb9e33940cb9
chrischall <chris.c.hall@gmail.com>
2026-09-21 11:42:04 -0400

    chore(main): release 3.1.0

--- packages/cli/src/version.ts
+++ packages/cli/src/version.ts
@@ -1,2 +1,2 @@
 // Managed by release-please (generic updater) — do not hand-edit.
-export const VERSION = '3.0.1'; // x-release-please-version
+export const VERSION = '3.1.0'; // x-release-please-version
